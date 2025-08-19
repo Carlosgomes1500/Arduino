5e3f92b430e07f1d409597b9654eaa7dc8f4eede
Carlosgomes1500 <Carlosgomes1500@hotmail.com>
2025-08-19 14:51:15 -0300

    Add files via upload

---
+++ 1.Um pouco mais sobre programação/1.Arrays/Ilumina__o_com_sequencial_de_LEDs/Ilumina__o_com_sequencial_de_LEDs.ino
@@ -0,0 +1,43 @@
+// --- Mapeamento do Hardware ---
+// Tempo de duração do pulso
+#define duraPulso 100 
+
+// Numero de pinos utilizados (ou seja, tamanho do array)
+#define numPins 6 
+
+// --- Variaveis utilizadas ---
+//Array de números de pinos aos quais os LEDs estão conectados 
+int ledPins[] = {  2, 3, 4, 5, 6, 7};
+
+ //Variavel auxiliar para controle da posição do array     
+int i;                                 
+
+// --- Configurações iniciais---
+void setup() 
+{
+  //loop para inicializar cada pino como uma saída
+  for (i = 0; i < numPins; i++)  
+  {
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+// --- Loop de progama---
+void loop() 
+{
+  // loop da sequencial dos LEDs do pino mais baixo para o mais alto: 
+  for (i = 0; i < numPins; i++)  
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(duraPulso);
+    digitalWrite(ledPins[i], LOW);
+  }
+
+   // loop da sequencial dos LEDs do pino mais alto para o mais baixo:
+ for (i = (numPins - 1); i >= 0; i--)  
+ {
+    digitalWrite(ledPins[i], HIGH);
+    delay(duraPulso );
+    digitalWrite(ledPins[i], LOW);
+  }
+}
